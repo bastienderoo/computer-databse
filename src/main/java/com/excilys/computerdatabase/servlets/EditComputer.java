3c58281796496e723faf98f88f2531a996d770d4
Deroo Bastien <bastien.deroo@2016.icam.fr>
2017-04-26 13:51:52 +0200

    [FEATURE] Editcomputer

---
+++ src/main/java/com/excilys/computerdatabase/servlets/EditComputer.java
@@ -0,0 +1,13 @@
+/**
+ * Created by excilys on 26/04/17.
+ */
+@javax.servlet.annotation.WebServlet(name = "EditComputer")
+public class EditComputer extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+
+    }
+}
