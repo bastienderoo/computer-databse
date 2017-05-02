40957e2a91071ea869de986c32a541061f776037
Deroo Bastien <bastien.deroo@2016.icam.fr>
2017-05-02 11:01:30 +0200

    [FEATURE] Gatling

--- src/test/java/com/excilys/computerdatabase/SeleniumTest.java
+++ src/test/java/com/excilys/computerdatabase/SeleniumTest.java
@@ -1,7 +1,45 @@
 package com.excilys.computerdatabase;
 
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by excilys on 28/04/17.
  */
 public class SeleniumTest {
+    private static WebDriver driver;
+    private final String homeDir = "LocalHost:8080/ComputerDatabase";
+    private WebElement element;
+    private static JavascriptExecutor js;
+
+
+    @BeforeClass
+    public static void setUp() {
+        System.setProperty("webdriver.firefox.driver", "lib/firefox");
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        js = (JavascriptExecutor) driver;
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        driver.close();
+    }
+
+    @Test
+    public void testNavigateToDashboard() {
+        driver.get(homeDir);
+        driver.findElement(By.id("goToDashborad")).click();
+        element = driver.findElement(By.id("dashboard"));
+        Assert.assertNotNull(element);
+    }
 }
