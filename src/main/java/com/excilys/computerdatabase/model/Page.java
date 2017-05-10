5778c47e75ab6955ec811b1ae283521e16eff0a8
Deroo Bastien <bastien.deroo@2016.icam.fr>
2017-05-10 18:20:00 +0200

    [RELEASE] DockerFiles

---
+++ src/main/java/com/excilys/computerdatabase/model/Page.java
@@ -0,0 +1,22 @@
+package com.excilys.computerdatabase.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by excilys on 10/05/17.
+ */
+public class Page {
+
+    int numberPage;
+    int elementByPage;
+    List<Computer> listElements = new ArrayList();
+
+    public Page(int numberPage, int elementByPage) {
+        this.numberPage = numberPage;
+        this.elementByPage = elementByPage;
+
+    }
+
+
+}
